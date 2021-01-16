bf8f57c040376e30a8856fcb71c6369d3649c0ba
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-16 09:12:06 +0000

    added a capitalize func

--- script.js
+++ script.js
@@ -41,3 +41,13 @@ console.log(findCompanies(itCompanies));
 
 loremIpsum =
 	"Contrary to popular belief, Lorem Ipsum is not simply random text. it has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.a.32 and 1.a.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. this book is a treatise on the theory of ethics, very popular during the Renaissance. the first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.a.32";
+
+const capitalize = (str) => {
+	let arr = str.split('. ');
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
+	}
+	return arr.join('. ');
+};
+
+console.log(capitalize(loremIpsum));
